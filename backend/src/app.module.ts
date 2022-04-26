f5e392c3ee3b2a4b5eec54443edeb09cf6a69c60
Stefan <stefan.djurovic@truepill.com>
2022-04-26 23:23:20 +0200

    env init

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(
+      process.env.DB_URI || 'mongodb://localhost/car-insurance',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
